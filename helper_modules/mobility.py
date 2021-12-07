00d024c34443abe587d7d2c653519b0324f77468
rwilson2 <rwilson2@andrew.cmu.edu>
2021-12-06 20:30:19 -0500

    Text updates and timeline changes

--- helper_modules/mobility.py
+++ helper_modules/mobility.py
@@ -3,7 +3,7 @@ from helper_modules.imports import *
 def run_mobility(apple_mobility_state, apple_mobility_time, google_national_data, google_state_data, mobility_groups):
     buff, col, buff2 = st.columns([1, 3, 1])
     col.header("How do you measure travel?")
-    col.markdown("")
+    col.markdown("In order to measure travel, we used two different datasets: Apple’s COVID-19 Mobility Trend Report and Google’s COVID-19 Community Mobility Report. The Apple dataset reports overall transit by the selected region as a percentage change from a baseline established on 13 January 2020. The Google dataset reports similar numbers, but is broken down by destination type (workplaces, parks, groceries and pharmacies, and retail and recreation). The Google dataset’s baseline was established between Jan 3 – Feb 6, 2020 and reports separate values for each day of the week. Neither dataset can properly account for holidays, local events, and seasonal differences.")
 
     plot_apple_data(apple_mobility_state, apple_mobility_time)
     plot_google_data(google_national_data, google_state_data, mobility_groups)
@@ -34,6 +34,9 @@ def plot_apple_data(apple_mobility_state, apple_mobility_time):
     fig.update_yaxes(showgrid=False, zeroline=False)
     col.plotly_chart(fig, use_container_width=True)
 
+    col.header("How has COVID-19 affected road transit?")
+    col.markdown("The plot above shows the change in overall transit at a national level from the Apple dataset. There is a clear decrease in transit following the initial wave of COVID-19 cases in the US in March 2020. However, the largest change in transit occurred during April 2020, following the state of national emergency declaration by President Trump and the shutdown of major public institutions by state governments starting in March 2020. These changes began to quickly recover however, until the second wave hit in late 2020. With this new wave starting in September 2020, transit levels declined once more until December 2020. Though, there may be some compounding impact caused by seasonal winter trends.")
+    col.markdown("After the second wave, the nation’s transit saw a continual upward trend until October 2021. Even during the Delta variant outbreak, transit rates continued to increase, though at a lower rate than both directly before and after. The return to normal transit numbers indicates that, in 2021, the U.S. population appears to be ignoring the impacts of COVID in their daily travel. While this behavior may be influenced by vaccination numbers, it also may show Americans have grown indifferent to COVID, potentially increasing the spread of future outbreaks. ")
     buff, col, buff2 = st.columns([5, 2, 5])
     yearOption = col.selectbox('Year', ('2020', '2021', '2020 and 2021'))
 
@@ -47,7 +50,8 @@ def plot_apple_data(apple_mobility_state, apple_mobility_time):
                          title="Percentage change in transit traffic compared to baseline (2019)"))
 
     col.plotly_chart(fig, use_container_width=True)
-
+    col.markdown("The above plot shows the Apple transit data grouped by state and maintains the overall trends of the national data. Wisconsin immediately pops out as an outlier during September-November 2021. This may be due to a combination of unseasonably warm weather in late 2021, along with the baseline being established in the middle of winter where transit would be relatively low [National Weather Service]. Another possible factor may have been events returning to full capacity for 2021, for example sporting events. While this effect may be less evident in larger states, Wisconsin has a low population with several  popular teams, such as the Packers. A sudden increase in the movement of football fans would be much more apparent in Wisconsin than in, say, California. ")
+    col.markdown("Another general trend is a higher transit rate among several Northeastern states, like New Jersey, Rhode Island, and Massachusetts, starting in the summer of 2021. Seasonal differences may be coming into play, as these states are typically hit harder by winter effects than the rest of the nation, resulting in a skewed baseline. ")
 
 def df_to_plotly_google(df):
     x = df.columns.tolist()
@@ -94,12 +98,17 @@ def plot_google_groups(mobility_groups):
     col.plotly_chart(fig, use_container_width=True)
 
 
+
 def plot_google_national(google_national_data, month_year):
     buff, col, buff2 = st.columns([1, 3, 1])
 
+    col.header("Are there any trends based on destination?")
+    col.markdown("The above plot shows the percent change in Google’s data in visits to workplaces, grocery stores, parks, and retail and recreation establishments compared to the pre-COVID baseline at a national level. This figure is interesting as it shows the distribution of transit across the nation since 2020. As expected, the number of visitors for each category dropped significantly in early 2020 as a response to the initial COVID wave and the restrictions that followed. However, the only category that remained below the baseline throughout the entirety of 2020 and 2021 is workplace transit. Retail and recreation visits remained below the baseline until mid-March 2021, but have remained above the baseline since. Similarly, grocery shop visits were above the baseline from May-August 2020, but have otherwise remained below the baseline throughout 2020 and up to mid-March 2021.")
+    col.markdown("Park visits are the largest outlier, as they have been significantly higher than the baseline throughout most of 2020 and 2021, with the exception of December 2020 - February 2021. However, this may be due to the baseline being established in January-February 2020 and does not account for seasonal trends. Generally, park visits would be significantly lower in the middle of the winter months. Parks are also the only category that is outdoors, and are likely not as heavily impacted by COVID restrictions. Overall, all of the plots have local minima correlating to the various outbreaks in the US. The largest decrease occurred in April 2020 with the initial outbreak, and another decrease in December 2020 due to the third and most severe outbreak. Smaller decreases can be observed during the second and Delta variant outbreaks during August 2020 and July 2021 respectively.")
+
     national_work_figure = px.line(
         google_national_data, x= month_year, y= google_national_data['workplaces_percent_change_from_baseline'],
-        title = 'Google Mobility - Workplace (National)',
+        title = 'National Workplace Mobility Percentage of Change',
         labels={
             'workplaces_percent_change_from_baseline':'Percent Change From Baseline',
             'x':'Date'
@@ -123,7 +132,8 @@ def plot_google_national(google_national_data, month_year):
     national_work_figure.update_yaxes(showgrid=False, zeroline=False)
 
     col.plotly_chart(national_work_figure, use_container_width=True)
-
+    col.header("Deep Dive into Workplace Transit")
+    col.markdown("The figure above shows the mobility changes for transit to and from workplaces from Google data at a national level. The figure shows similar trends to the Apple overall transit data in early 2020, but diverges significantly during 2021. However, the two diverge significantly in 2021. The Apple data indicates overall transit has recovered to pre-COVID levels by late 2021, while the Google workplace transit data indicates that travel to and from work has remained steadily below baseline levels. As stated previously, this may indicate that the population’s response to COVID has largely died down, though employers may still be following manning protocols. However, this may also be due to other factors such as unemployment and remote work becoming more accepted and facilitated. An interesting side-effect of COVID may be remote work being offered in more jobs moving forward, with transit rates never fully returning to pre-COVID levels.")
 
 def plot_google_state(google_state_data, month_year):
     buff, col, buff2 = st.columns([5, 2, 5])
@@ -134,19 +144,21 @@ def plot_google_state(google_state_data, month_year):
     buff, col, buff2 = st.columns([1, 3, 1])
     stateWork = go.Figure(
                 data=go.Heatmap(df_to_plotly_google(google_state_data[month_year]), type = 'heatmap', colorscale = 'rdbu'),
-                layout=go.Layout(width = 600,height = 1000, title="Google Mobility - Percent Change in Workplace Mobility (By State)"))
+                layout=go.Layout(width = 600,height = 1000, title="Percent Change in Workplace Mobility By State"))
     stateWork.update_layout(
         xaxis_title='Date',
         yaxis_title= 'State')
 
     col.plotly_chart(stateWork, use_container_width=True)
+    col.markdown("The heat map above shows a by-state breakdown of the workplace mobility trends over the same time period as the national plot. Overall, the trends follow those of the national data, with decreases during each wave and general recovery to a steady state in between waves. As a general trend, low population density states such as Missouri, Nebraska, Montana, and Idaho tend to recover faster. Of note, some states seem to recover at slower rates than others. Hawaii’s recovery is likely due to its geography as a set of islands isolated from the rest of the continental United States.")
+    col.markdown("California is an interesting outlier, as though the state is very large by area, 95% of the population resides in densely populated urban clusters [US Census Bureau].  Similarly, New Jersey also lags behind in mobility recovery and is the 2nd highest state in terms of urban population percentage. The north east as a whole is highly urbanized, and correspondingly, several outlier states that lag behind are in that region such as Massachusetts, Rhode Island, and Vermont.")
     dict_check = col.checkbox("Citations")
     if dict_check:
         col.header("Citations:")
         col.markdown("Apple. (2021, November). COVID‑19 - Mobility Trends Reports. https://covid19.apple.com/mobility")
         col.markdown("Google. (2021, November). See how your community is moving around differently due to COVID-19. https://www.google.com/covid19/mobility/index.html?hl=en)")
+        col.markdown("National Weather Service. (2021, September). Very Dry and Warm across Southern Wisconsin in September. Weather. https://www.weather.gov/mkx/septemberclimate")
         col.markdown("US Census Bureau Public Information Office. (2012, March 26). Growth in Urban Population Outpaces Rest of Nation, Census Bureau Reports - 2010 Census - Newsroom - U.S. Census Bureau. Census. ")
-
 
 
 def plot_google_data(google_national_data, google_state_data, mobility_groups):
--- helper_modules/power.py
+++ helper_modules/power.py
@@ -3,6 +3,8 @@ from helper_modules.imports import *
 
 def energy_timeline_graph(energy_change_national, energy_change_states):
     buff, col, buff2 = st.columns([1, 3, 1])
+    col.header("How do you measure Power?")
+    col.markdown("We used the U.S. Energy Information Administration’s dataset for the total electric power industry for 2019-2022. This dataset reports electricity generation reported in Megawatt Hours. We subtracted monthly energy generation values from 2020 and 2021 from the 2019 data, and divided by the 2019 value to calculate the percent change by month for each state.")
 
     fig = px.line(energy_change_national, x='Date',y='change', markers=True)
     fig.add_vrect(x0="9.5",
@@ -27,7 +29,9 @@ def energy_timeline_graph(energy_change_national, energy_change_states):
                       yaxis_title='% Change')
     col.plotly_chart(fig, use_container_width=True)
 
-    col.header("How has COVID-19 affected Energy Generation in various states?")
+    col.header("Has power generation changed due to COVID-19?")
+    col.markdown("The above figure shows the change in national electric power generation per month compared to a baseline value from 2019. Overall, total energy generation remained slightly lower than that of 2019.  There was a distinct decrease between March-May 2020, corresponding to the initial spread of COVID-19 in the US. After this minimum value, the plot generally trends upwards, indicating a gradual recovery. This may also indicate that electricity requirements are gradually increasing from year to year.")
+    col.markdown("The main driver of monthly variation appears to be seasonal trends, with spikes during winter and summer months. However, COVID-19 cases do appear to be playing a role as the rate of power generation increases during the period of the largest wave of COVID cases during the winter of 2020 and the Delta variant outbreak in the summer of 2021. This may be due to an increase in residential power consumption for climate control, without a corresponding decrease in workplace power consumption as their climate control needs do not change. The monthly fluctuations remain relatively small, as national energy requirements should not change drastically and stay at home policies would only shift energy demand from commercial to residential.")
 
 
     colors = {
@@ -55,6 +59,8 @@ def energy_timeline_graph(energy_change_national, energy_change_states):
     fig.layout.plot_bgcolor = '#121212'
     fig.layout.paper_bgcolor = '#121212'
     col.plotly_chart(fig, use_container_width=True)
+    col.header("Are there any trends in state electricity generation?")
+    col.markdown("The above plot shows the top 5 states that experienced the largest mean differences for both negative and positive energy generation since 2019. In general, the states with low energy consumption per capita tended to have higher energy generation than the baseline and vice versa [EIA]. The outliers to this trend are Massachusetts and Rhode Island, as both have relatively low energy consumption per capita but also have large negative changes in energy generation. There may also be a regional trend, as both states are in the northeast.")
 
     # col.subheader(
     #     'Observing change in usage on a monthly basis, on positive and negative states'
@@ -90,6 +96,8 @@ def plot_energy_heatmap(energy_change_states_heatmap):
                          title='Total Electric Power Industry')
     )
     col.plotly_chart(fig, use_container_width=True)
+    col.markdown("The figure shows the percent change in electricity generation by state from 2020-2021. Generally, the patterns follow those of the national data. This plot highlights the two outliers identified in the previous plot. Rhode Island is the lowest energy consumer by capita, one of the highest percent of residential electricity consumption, and very little commercial and industrial electricity consumption [EIA]. Therefore, a stay at home policy would impact Rhode Island more heavily than other states.")
+    col.markdown("On the other hand, Massachusetts electricity generation has consistently been below its 2019 value. Massachusetts consumes 15 times more energy than it generates, with half of its energy consumed by the commercial sector [EIA]. The compounding effect of a low baseline generation value and changes to the commercial energy consumption due to COVID-19 may explain its drastic difference from the 2019 value. Overall, individual state analysis of electricity generation heavily depends on energy profiles and national trends should be not applied without further analysis.")
 
     dict_check = col.checkbox("Citations")
 
@@ -99,6 +107,5 @@ def plot_energy_heatmap(energy_change_states_heatmap):
 
 
 def run_power( energy_change_national, energy_change_states, energy_change_states_heatmap):
-    st.title('Power')
     energy_timeline_graph(energy_change_national, energy_change_states)
     plot_energy_heatmap(energy_change_states_heatmap)
